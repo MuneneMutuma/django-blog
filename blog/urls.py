920fe992c63da93ae9e27d281952f3f60b83a584
Munene Mutuma <munene.work@gmail.com>
2022-02-08 16:14:55 +0300

    Pagination

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,8 @@ from .views import (
     PostDetailView,
     PostCreateView,
     PostUpdateView,
-    PostDeleteView)
+    PostDeleteView,
+    UserPostListView)
 from . import views
 
 urlpatterns = [
@@ -13,6 +14,7 @@ urlpatterns = [
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
     path('about/', views.about, name="blog-about"),
 
     
